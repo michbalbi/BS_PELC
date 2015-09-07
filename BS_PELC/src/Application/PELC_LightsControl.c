2661407240d911a0b531588a83c2d18c3480de1e
michbalbi <michbalbi@users.noreply.github.com>
2015-09-07 10:03:37 -0500

    Added missing ADC and PWM libs. First implementation of funcs in PELC_LightsControl.c module.

--- BS_PELC/src/Application/PELC_LightsControl.c
+++ BS_PELC/src/Application/PELC_LightsControl.c
@@ -3,27 +3,30 @@
 /*============================================================================*/
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*
-* C Source:         %MPC5606B_PWM_lib.c%
-* Instance:         1
-* %version:         1 %
+* C Source:         %PELC_LightsControl.c%
+* Instance:         x
+* %version:         x %
 * %created_by:      Michele Balbi %
-* %date_created:    August 31 2015 %
+* %date_created:    2015 %
 *=============================================================================*/
-/* DESCRIPTION : C source file for PWM functionality.                         */
+/* DESCRIPTION : C source template file                                       */
 /*============================================================================*/
-/* FUNCTION COMMENT : This file describes the C source file used to configure */
-/* the PWM functionality through the OPWMFB mode in the EMIOS module.         */
+/* FUNCTION COMMENT : This file describes the C source template according to  */
+/* the new software platform                                                  */
+/*                                                                            */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*  REVISION |   DATE      |                               |      AUTHOR      */
 /*----------------------------------------------------------------------------*/
-/*  1.0      | 31/08/2015  |                               | Michele Balbi    */
-/* 	First draft.    			                                              */
+/*  1.0      | DD/MM/2015  |                               | Michele Balbi    */
+/* 								                                              */
 /*============================================================================*/
 
 /* Includes */
 /* -------- */
+#include "PELC_LightsControl.h"
+#include "MPC5606B_GPIO_lib.h"
 #include "MPC5606B_PWM_lib.h"
 
 /* Functions macros, constants, types and datas         */
@@ -53,8 +56,12 @@
 
 
 /* LONG and STRUCTURE RAM variables */
+MAIN_LIGHTS_CMD_STATE re_MainLightsCmdState = E_OFF;
+TURN_CMD_STATE re_TurnCmdState = E_TURN_INACTIVE;
+HAZARD_CMD_STATE re_HazardCmdState = E_HAZARD_INACTIVE;
+STOP_CMD_STATE re_StopCmdState = E_STOP_INACTIVE;
 
-
+/*extern CAN_MESSAGE_HANDLER_STATE re_CANMsgHandlerState;*/
 /*======================================================*/ 
 /* close variable declaration sections                  */
 /*======================================================*/ 
@@ -88,38 +95,59 @@
  *  Return               :
  *  Critical/explanation :    [yes / No]
  **************************************************************/
-void PWM_Disable(void){
-	EMIOS_0.MCR.B.FRZ = 1; 			/* Enable channel freezing */
-	EMIOS_0.MCR.B.GTBE = 0;			/* Global Time Base Disable  */
-	EMIOS_0.MCR.B.GPREN = 0;		/* 1. Global Prescaler Disabled */
+void PELC_TurnOnFull(T_UBYTE lub_pcr){
+	GPIO_AS_OUTPUT(lub_pcr);
+	OUTPUT_HIGH(lub_pcr);
 }
 
-void PWM_Enable(T_UBYTE lub_GlobalPrescaler){
-	EMIOS_0.MCR.B.GPRE = lub_GlobalPrescaler;		/* Global Prescaler */
-	
-	/* Now, enable everything back*/
-	EMIOS_0.MCR.B.GPREN = 1;
-	EMIOS_0.MCR.B.GTBE = 1;
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+void PELC_TurnOff(T_UBYTE lub_pcr){
+	GPIO_AS_OUTPUT(lub_pcr);
+	OUTPUT_LOW(lub_pcr);
+}
+
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+void PELC_TurnOnLevel(T_UBYTE lub_pcr, T_UBYTE lub_Channel, T_UBYTE lub_DutyCycle, T_UBYTE lub_Freq){
+	GPIO_ALT_MODE_1(lub_pcr);
+	PWM_Channel_Set(lub_Channel, lub_DutyCycle, lub_Freq);
+}
+
+
+/*
+Sch Tasks are 1.25, 5, 10 and 40MS
+*/
+void PELC_Task_1P25MS(void){
 	
 }
 
-void PWM_Channel_Init(T_UBYTE lub_Channel, T_UBYTE lub_ChPrescaler, T_UBYTE lub_DutyCycle, T_UBYTE lub_Freq){
-	EMIOS_0.CH[lub_Channel].CCR.B.MODE = 1;						/* GPIO output mode for CH[x] */
-	EMIOS_0.CH[lub_Channel].CCR.B.FREN = 1; 					/* CH[x] is now frozen */
+void PELC_Task_5MS(void){
+	/*if(re_CANMsgHandlerState == E_MSG_RECEIVED){
+		PELC_ProcessCANMsg();
+	}*/
+}
+
+void PELC_LightSwitch_100MS(void){
+	/*if(){
+		
+	}*/
+}
+
+void PELC_SendStatus_100MS(void){
 	
-	/* Steps using Reference Manual guide */
-	EMIOS_0.CH[lub_Channel].CCR.B.UCPEN = 0;					/* 2. Channel Prescaler Disabled */
-	
-	EMIOS_0.CH[lub_Channel].CCR.B.BSL = 0x3; 					/*    Bus Selection to internal counter for CH[0] */
-	EMIOS_0.CH[lub_Channel].CCNTR.R = 2; 						/* 3. Internal Counter initial value */
-	EMIOS_0.CH[lub_Channel].CCR.B.UCPRE = lub_ChPrescaler - 1u;	/*    Channel prescaler set to 4 */
-	EMIOS_0.CH[lub_Channel].CADR.R = lub_DutyCycle;				/* 4. Set A register */
-	EMIOS_0.CH[lub_Channel].CBDR.R = lub_Freq;					/* 4. Set B register */
-	
-	EMIOS_0.CH[lub_Channel].CCR.B.FREN = 0;
-	
-	EMIOS_0.CH[lub_Channel].CCR.B.UCPEN = 1;
-	
-	EMIOS_0.CH[lub_Channel].CCR.B.MODE = 0x58; 	/* OPWFMB mode, 0101 1000 */
+}
+
+void PELC_Task_500MS(void){
 	
 }
--- BS_PELC/src/Application/PELC_LightsControl.h
+++ BS_PELC/src/Application/PELC_LightsControl.h
@@ -3,32 +3,31 @@
 /*============================================================================*/
 /*                        OBJECT SPECIFICATION                                */
 /*============================================================================*
-* C Include:        %MPC5606B_PWM_lib.h%
-* Instance:         1
-* %version:         1 %
+* C Include:        %PELC_LightsControl.h%
+* Instance:         x
+* %version:         x.x %
 * %created_by:      Michele Balbi %
-* %date_created:    August 31 2015 %
+* %date_created:    2015 %
 *=============================================================================*/
-/* DESCRIPTION : Header file for PWM functionality.                           */
+/* DESCRIPTION : Header file for 				                              */
 /*============================================================================*/
 /* FUNCTION COMMENT :   										              */
+/*                                                                            */
 /*============================================================================*/
 /*                               OBJECT HISTORY                               */
 /*============================================================================*/
 /*  REVISION |   DATE      |                               |      AUTHOR      */
 /*----------------------------------------------------------------------------*/
-/*  1.0      | 31/08/2015  |                               | Michele Balbi    */
-/* First Draft.  															  */
+/*  1.0      | DD/MM/2015  |                               | Michele Balbi    */
+/* First Draft 																  */
 /*============================================================================*/
 
-#ifndef MPC5606B_PWM_LIB_H              /* To avoid double inclusion */
-#define MPC5606B_PWM_LIB_H
+#ifndef PELC_LIGHTS_CTRL_H                               /* To avoid double inclusion */
+#define PELC_LIGHTS_CTRL_H
 
 /* Includes */
 /* -------- */
-#include "MPC5606B.h"
-#include "conti_typedefs.h"
-/*#include "MPC5606B_GPIO_lib.h"*/
+
 
 /* Exported types and constants */
 /* ---------------------------- */
@@ -36,6 +35,37 @@
 /* Types definition */
 /* typedef */
 
+typedef enum {
+	E_STD,
+	E_HIGH,
+	E_LUX
+}HW_CONFIG;
+
+typedef enum {
+	E_OFF,
+	E_DAY,
+	E_FULL,
+	E_AUTO
+}MAIN_LIGHTS_CMD_STATE;
+
+typedef enum {
+	E_TURN_INACTIVE,
+	E_TURN_ACTIVE_ON_RIGHT,
+	E_TURN_ACTIVE_OFF_RIGHT,
+	E_TURN_ACTIVE_ON_LEFT,
+	E_TURN_ACTIVE_OFF_LEFT
+}TURN_CMD_STATE;
+
+typedef enum {
+	E_HAZARD_INACTIVE,
+	E_HAZARD_ACTIVE_ON,
+	E_HAZARD_ACTIVE_OFF
+}HAZARD_CMD_STATE;
+
+typedef enum {
+	E_STOP_INACTIVE,
+	E_STOP_ACTIVE
+}STOP_CMD_STATE;
 
 /*==================================================*/ 
 /* Declaration of exported constants                */
@@ -70,47 +100,21 @@
 /* ---------------------------------------- */
 
 /* Functions prototypes */
-extern void PWM_Channel_Init(T_UBYTE lub_Channel, T_UBYTE lub_ChPrescaler, T_UBYTE lub_DutyCycle, T_UBYTE lub_Freq);
-extern void PWM_Enable(T_UBYTE lub_GlobalPrescaler);
-extern void PWM_Disable(void);
+
 
 /* Functions macros */
 
 
 /* Exported defines */
-#define EMIOS_0_CH0  0
-#define EMIOS_0_CH1  1
-#define EMIOS_0_CH2  2
-#define EMIOS_0_CH3  3
-#define EMIOS_0_CH4  4
-#define EMIOS_0_CH5  5
-#define EMIOS_0_CH6  6
-#define EMIOS_0_CH7  7
-#define EMIOS_0_CH8  8
-#define EMIOS_0_CH16 16
-#define EMIOS_0_CH23 23
-#define EMIOS_0_CH24 24
 
-#define EMIOS_0_CH0_PCR  0
-#define EMIOS_0_CH1_PCR  1
-#define EMIOS_0_CH2_PCR  2
-#define EMIOS_0_CH3_PCR  3
-#define EMIOS_0_CH4_PCR  4
-#define EMIOS_0_CH5_PCR  5
-#define EMIOS_0_CH6_PCR  6
-#define EMIOS_0_CH7_PCR  7
-#define EMIOS_0_CH8_PCR  8
-#define EMIOS_0_CH16_PCR 64
-#define EMIOS_0_CH23_PCR 71
-#define EMIOS_0_CH24_PCR 75
+#define PARKL_FL_PWMCH		0
+#define BLINKERL_FL_PWMCH	1
+#define PARKL_FR_PWMCH		0
+#define BLINKERL_FR_PWMCH	1
+#define PARKL_RL_PWMCH		4
+/*#define STOPL_RL_*/
 
-/*#define EMIOS_0_GLOBAL_PRESCALER 255u
-#define EMIOS_0_FREQ			 62500u
-#define EMIOS_0_CH_PRESCALER	 (MCU_FREQ/((EMIOS_0_GLOBAL_PRESCALER+1)*EMIOS_0_FREQ))
-#define PWM_FREQ 	 			 1000u
-#define PWM_B_REG				 (EMIOS_0_FREQ/PWM_FREQ)
-#define PWM_DUTY_CYCLE			 50u
-#define PWM_A_REG				 ((PWM_DUTY_CYCLE/100)*PWM_B_REG)*/
+
 
 #endif
 
